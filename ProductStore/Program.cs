e338a7a32edd7cdb7d4d633d42eee39a26d92b05
Sergio <sergio.kogut@gmail.com>
2018-05-02 14:02:45 +0200

    next

--- ProductStore/Program.cs
+++ ProductStore/Program.cs
@@ -3,13 +3,77 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+/*
+ Разработать архитектуру классов иерархии товаров
+при разработке системы управления потоками товаров для
+дистрибьюторской компании. Прописать члены классов.
+Создать диаграммы взаимоотношений классов.
+Должны быть предусмотрены разные типы товаров,
+в том числе:
+• бытовая химия;
+• продукты питания.
+Предусмотреть классы управления потоком товаров
+(пришло, реализовано, списано, передано).
+ */
+
 
 namespace ProductStore
 {
+
+
+    abstract class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public abstract void ShowInformation();
+        public Product(string name = "noname", double price = 0.00, int quantity = 0)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public double GetTotalValue()
+        {
+            return Price * Quantity;
+        }
+
+    }
+
+    class Food : Product
+    {
+        public override void ShowInformation()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class HouseholdChemicals : Product
+    {
+
+        public override void ShowInformation()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+    class StoreProduct
+    {
+
+
+    }
+
+
+
+
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
