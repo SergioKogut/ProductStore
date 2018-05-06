b603d2bfa167d219f275a434b10ccbfff237ce7f
Sergio <sergio.kogut@gmail.com>
2018-05-06 22:41:50 +0200

    next

--- ProductStore/Program.cs
+++ ProductStore/Program.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IMenu = System.Collections.Generic.List<System.Tuple<string, GetMethod>>;
+using ItemMenu = System.Tuple<string, GetMethod>;
+using MenuSpace;
+public delegate void GetMethod();
+
 /*
  Разработать архитектуру классов иерархии товаров
 при разработке системы управления потоками товаров для
@@ -16,23 +21,31 @@ using System.Threading.Tasks;
 (пришло, реализовано, списано, передано).
  */
 
-
 namespace ProductStore
 {
-
+    enum ProductType { NoType, Food, Сlothes, Jewelry, Electronics, Toys, Furniture, HouseholdChemical }
 
     abstract class Product
     {
         public string Name { get; set; }
         public double Price { get; set; }
         public int Quantity { get; set; }
+        public ProductType Type { get; set; }
+        public DateTime DateExpiration { get; set; }
+
 
         public abstract void ShowInformation();
-        public Product(string name = "noname", double price = 0.00, int quantity = 0)
+        public Product(string name = "noname",
+                       double price = 0.00,
+                       int quantity = 0,
+                       ProductType type = ProductType.NoType,
+                       DateTime dateexp = new DateTime())
         {
             Name = name;
             Price = price;
             Quantity = quantity;
+            Type = type;
+            DateExpiration = dateexp;
         }
 
         public double GetTotalValue()
@@ -62,18 +75,61 @@ namespace ProductStore
 
     class StoreProduct
     {
+        private Menu Menu1;
+        private IMenu Item1;
 
+        public StoreProduct()
+        {
+            Item1 = new IMenu
+            {
+                new ItemMenu(" Добавить товар", new GetMethod(Receipt)),
+                new ItemMenu(" Реализавать товар", new GetMethod(Selling)),
+                new ItemMenu(" Списать товар", new GetMethod(Cancellation)),
+                new ItemMenu(" Выход", new GetMethod(Exit))
+            };
+
+            Menu1 = new Menu(5, 5, Item1);
+
+        }
+        private void Receipt()
+        {
+            Console.WriteLine("Добавить товар");
+        }
+
+        private void Selling()
+        {
+            Console.WriteLine("Реализавать товар");
+        }
+        private void Cancellation()
+        {
+            Console.WriteLine("Списать товар");
+        }
+        private void Exit()
+        {
+            Console.WriteLine("Выход");
+        }
+
+        public void Run()
+        {
+            Menu1.Show();
+        }
 
     }
-
-
 
 
     class Program
     {
+
         static void Main(string[] args)
         {
 
+            StoreProduct store = new StoreProduct();
+            store.Run();
+
+            Console.ReadKey();
+
         }
+
     }
 }
+
