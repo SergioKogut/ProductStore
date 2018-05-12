377fddc7420952569761f7382d6968319a7da276
Sergio <sergio.kogut@gmail.com>
2018-05-12 19:36:02 +0200

    next

--- ProductStore/Program.cs
+++ ProductStore/Program.cs
@@ -36,17 +36,19 @@ namespace ProductStore
 
         public abstract void ShowInformation();
         public Product(string name = "noname",
+                       ProductType type = ProductType.NoType,
                        double price = 0.00,
                        int quantity = 0,
-                       ProductType type = ProductType.NoType,
                        DateTime dateexp = new DateTime())
         {
             Name = name;
+            Type = type;
             Price = price;
             Quantity = quantity;
-            Type = type;
             DateExpiration = dateexp;
         }
+
+
 
         public double GetTotalValue()
         {
@@ -57,10 +59,27 @@ namespace ProductStore
 
     class Food : Product
     {
+
+        public Food(string name = "noname",
+                       ProductType type = ProductType.NoType,
+                       double price = 0.00,
+                       int quantity = 0,
+                       DateTime dateexp = new DateTime()):base(name, type, price, quantity, dateexp)
+        { }
+
+
         public override void ShowInformation()
         {
-            throw new NotImplementedException();
+            {
+                Console.WriteLine($"Product:  Name:{Name,15}" +
+                                  $"          Type:{Type,10}" +
+                                  $"          Price: {Price,12}grn " +
+                                  $"          Quantity: {Quantity,5} p " +
+                                  $"          Date expiration {DateExpiration,10} ");
+            }
         }
+
+
     }
 
     class HouseholdChemicals : Product
@@ -73,15 +92,18 @@ namespace ProductStore
 
     }
 
-    class StoreProduct
+    class StoreProduct : List<Product>
     {
         private Menu Menu1;
         private IMenu Item1;
-
+        private bool ExitFlag = true;
+       
+       
         public StoreProduct()
         {
             Item1 = new IMenu
             {
+                new ItemMenu(" Вывести список товаров", new GetMethod(Print)),
                 new ItemMenu(" Добавить товар", new GetMethod(Receipt)),
                 new ItemMenu(" Реализавать товар", new GetMethod(Selling)),
                 new ItemMenu(" Списать товар", new GetMethod(Cancellation)),
@@ -91,9 +113,54 @@ namespace ProductStore
             Menu1 = new Menu(5, 5, Item1);
 
         }
+        private void Print()
+        {
+            Console.WriteLine(new string('-', 80));
+            foreach (var product in this)
+            {
+                product.ShowInformation();
+            }
+
+            Console.WriteLine(new string('-', 80));
+            Console.Read();
+            Console.Clear();
+        }
+        
+           
+            
+        
         private void Receipt()
         {
-            Console.WriteLine("Добавить товар");
+            Console.WriteLine("ДОБАВИТЬ ТОВАР:");
+            double price;
+            int quantity;
+            DateTime dateexp;
+            Console.Write("Введите название товара:");
+            string name = Console.ReadLine();
+            ProductType type = ProductType.NoType;
+            do
+            {
+                Console.Write("Введите цену товара:");
+            } while (!double.TryParse(Console.ReadLine(), out price));
+
+            do
+            {
+                Console.Write("Введите количество товара:");
+            } while (!Int32.TryParse(Console.ReadLine(), out quantity));
+
+            /*
+            do
+            {
+                Console.Write("Введите дату изготовление:");
+            } while (!DateTime.TryParse(Console.ReadLine(), out dateexp));
+            */
+
+            dateexp= DateTime.Now;
+
+            Product temp = new Food(name, type, price, quantity, dateexp);
+
+            this.Add(temp);
+            Console.Clear();
         }
 
         private void Selling()
@@ -107,11 +174,18 @@ namespace ProductStore
         private void Exit()
         {
             Console.WriteLine("Выход");
+            ExitFlag = false;
         }
 
         public void Run()
         {
-            Menu1.Show();
+            do
+            {
+                Menu1.Show();
+
+            } while (ExitFlag);
+
+            
         }
 
     }
