f33158eea4d467598391cd483e924cf84e73b40a
Sergio <sergio.kogut@gmail.com>
2018-05-13 01:33:34 +0200

    next

--- ProductStore/menu.cs
+++ ProductStore/menu.cs
@@ -6,6 +6,72 @@ using System.Threading.Tasks;
 
 namespace MenuSpace
 {
+    /*
+      Клас  горизонтального меню   
+    */
+
+    class HorizontalMenu
+    {
+        private List<string> MenuItem;
+        private int ActiveItem = 0;
+        private ConsoleKeyInfo key;
+        private int PosXmenu = Console.CursorLeft; 
+        private int PosYmenu = Console.CursorTop;
+        
+        public HorizontalMenu(List<string> temp)
+        {
+            MenuItem = temp;
+            
+        }
+
+        private void SetColorText(int x, int y, string text, ConsoleColor color)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.ForegroundColor = color;
+            Console.Write(text);
+        }
+
+        public string Show()
+        {
+            do
+            {
+                //друк меню
+                
+                    for (int i = 0; i < MenuItem.Count; i++)
+                    {
+                        if (i == ActiveItem)
+                        {
+                            SetColorText(PosXmenu + i*10, PosYmenu , MenuItem[i], ConsoleColor.Yellow);
+                        }
+                        else
+                        {
+                            SetColorText(PosXmenu +i*10, PosYmenu , MenuItem[i], ConsoleColor.Green);
+                        }
+                    }
+                //перевірка нажаття клавіш
+                key = Console.ReadKey();
+                if (key.Key == ConsoleKey.RightArrow && ActiveItem < MenuItem.Count - 1)
+                  ActiveItem++;
+                if (key.Key == ConsoleKey.LeftArrow && ActiveItem > 0)
+                  ActiveItem--;
+                
+
+            }//виході якщо нажати Ентер
+            while (key.Key != ConsoleKey.Enter);
+            Console.ForegroundColor= ConsoleColor.Green;
+            Console.WriteLine();
+            return MenuItem[ActiveItem]; // вертаємо значення горизонтального меню
+
+        }
+    }
+
+
+    /*
+      Клас  вертикального меню   
+    */
+
+
+
     class Menu
     {
 
@@ -16,23 +82,14 @@ namespace MenuSpace
         private GetMethod Method;
         private List<Tuple<string, GetMethod>> MenuItem;
         private ConsoleKeyInfo key;
-        public Menu()
-        {
 
-            MenuItem = new List<Tuple<string, GetMethod>>
-            {
-                new Tuple<string, GetMethod>("Del", Del),
-                new Tuple<string, GetMethod>("Ask", Ask)
-            };
-        }
-
+        public Menu() {}
         public Menu(int x, int y, List<Tuple<string, GetMethod>> temp)
         {
             PosXmenu = x;
             PosYmenu = y;
             MenuItem = temp;
         }
-
         private void SetColorText(int x, int y, string text, ConsoleColor color)
         {
             Console.SetCursorPosition(x, y);
@@ -42,10 +99,9 @@ namespace MenuSpace
 
         public void Show()
         {
-           
-                do
+            do
             {
-
+                
                 //друк меню
                 for (int i = 0; i < MenuItem.Count; i++)
                 {
@@ -58,7 +114,7 @@ namespace MenuSpace
                         SetColorText(PosXmenu, PosYmenu + i, MenuItem[i].Item1, ConsoleColor.Green);
                     }
                 }
-
+                SetColorText(PosXmenu, PosYmenu-1, "  МЕНЮ", ConsoleColor.Green);
                 //перевірка нажаття клавіш
                 key = Console.ReadKey();
                 if (key.Key == ConsoleKey.DownArrow && ActiveItem < MenuItem.Count - 1)
@@ -76,23 +132,11 @@ namespace MenuSpace
             Method = MenuItem[ActiveItem].Item2;
             Method.Invoke();
         }//вихді якщо нажади Ентер
-        
-
+   
         public void Add(Tuple<string, GetMethod> item)
-        {
-            MenuItem.Add(item);
-        }
-
-        private void Ask()
-        {
-            Console.WriteLine("Method Ask");
-
-        }
-        private void Del()
-        {
-            Console.WriteLine("Method Del");
-
-        }
+            {
+                MenuItem.Add(item);
+            }
 
     }
 }
